a4eea3f4c1034a851bc03108c1093102654ff6b6
unknown <swarudi555@gmail.com>
2024-01-03 12:41:25 +0530

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* Before 7 to 8 years there were no arrow functions and many other conventions
+that we used in today js so to make the new and old code both compatible 
+to the v8 engine or any other browser engine we use "use strict"*/
+
+// alert("hello") // we are using node js this can be executed in browser.
+
+/*we can know the codeing standards of javascript by using mdn which is of mozilla 
+brower it is easy and beginner friendly but the orignal documnetation in given in
+tc39.es that is ecma script */
+
+let name = "Rudraksh";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to the power of 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => value is still not defined
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
